c573b795dbb9d748c2c9ade504c8c743c468e975
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2025-02-21 14:49:04 -0300

    IMC PROJECT

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JS-MINI-PROJECT-IMC
+# JS-MINI-PROJECTS
 This mini project is a initiative mine to learn JavaScript. The focus here is to understand better how can i use JS and, in posteriory, to integrate it on my projects Python with Django.
 
 I find out this project on YouTube channel called [Fernando Leonid](https://www.youtube.com/watch?v=RacwEvoTz_Y&list=PLDgemkIT111AzoS1rB61sgMJbsEA4pyD2). I congratualiton the owner of the channel for help others persons by sharing you knowledgment.
---
+++ imc.js
@@ -0,0 +1,58 @@
+// Here, we catch out the tags that will be used on our script and put it on variables.
+
+const calcular = document.getElementById('calcular')
+
+function imc () {
+
+    const nome = document.getElementById('nome').value
+    const altura = document.getElementById('altura').value
+    const peso = document.getElementById('peso').value
+    const resultado = document.getElementById('resultado')
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        
+        const valorIMC = (peso/ (altura * altura)).toFixed(2);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5) {
+            classificacao = 'You are below the weight.';
+        } else if (valorIMC < 25) {
+            classificacao = 'You are with the ideal weight.';
+        } else if (valorIMC < 30) {
+            classificacao = 'You are bit above of the weight.';
+        } else if (valorIMC < 35) {
+            classificacao = 'You are obesity level I.';
+        } else if (valorIMC < 40) {
+            classificacao = 'You are obesity level II.';
+        } else {
+            classificacao = 'You are obesity level III. Careful!';
+        }
+        
+
+        resultado.textContent = `${nome}, your IMC is ${valorIMC}. ${classificacao}`;
+
+    } else {
+        resultado.textContent = 'Fill out all of the fields!';
+    }
+
+}
+
+calcular.addEventListener('click', imc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* playing with the code */
+document.write(nome.value) 
