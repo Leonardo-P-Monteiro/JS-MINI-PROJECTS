3a470ca9bf40f574edfeb01eabcb9d2e501a65db
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2025-02-25 15:11:10 -0300

    CONCLUSION OF PROJECT TRAFFIC LIGHT

--- TRAFFIC LIGHT/trafficlight.js
+++ TRAFFIC LIGHT/trafficlight.js
@@ -1,7 +1,11 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
+let colorIndex = 0;
+let IntervalId = null;
+
 
 const trafficLight = ( event ) => {
+    stopAutomatic()
     turnOn[event.target.id]();
 
 }
@@ -9,8 +13,29 @@ const trafficLight = ( event ) => {
 const turnOn = {
     'red': () => img.src = './image/vermelho.png',
     'green': () => img.src = './image/verde.png',
-    'yellow': () => img.src = './image/amarelo.png',    
+    'yellow': () => img.src = './image/amarelo.png', 
+    'automatic': () => IntervalId = setInterval(changeColor, 1000),
 }
 
+const nextIndex = () => {
+    if (colorIndex < 2) {
+        colorIndex++ 
+    } else {
+        colorIndex = 0;
+    }
+}
+
+const stopAutomatic = () => {
+    clearInterval(IntervalId)
+}
+
+const changeColor = () => {
+    const colors = ['red', 'green', 'yellow'];
+    const color = colors[ colorIndex ];
+    turnOn[color]();
+    nextIndex()
+
+
+}
 
 buttons.addEventListener('click', trafficLight);
