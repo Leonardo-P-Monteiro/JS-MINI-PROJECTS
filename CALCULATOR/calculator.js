d68168e1b19005ffa55e71a6826deb13ab2a5ce7
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2025-03-20 15:52:16 -0300

    Project Calculator
    We stoped on the 31 minutes on thi video.

--- CALCULATOR/calculator.js
+++ CALCULATOR/calculator.js
@@ -5,14 +5,31 @@ const display = document.getElementById('display');
 const numeros = document.querySelectorAll('[id*=tecla]');
 const operadores = document.querySelectorAll('[id*=operador]');
 
-let novoNumero = true
+let novoNumero = true;
+let input1; // numeroAnterior
+let operador;
+
+const operacaoPendente = () => {
+    operador != undefined;
+    return operador
+}
+
+const calcular = () => {
+    if (operacaoPendente()){
+        const input2 = parseFloat(display.textContent);
+        novoNumero = true
+        if (operador == '+') {
+            atualizarDisplay(input1 + input2);
+        }
+    }
+}
 
 const atualizarDisplay = (texto) => {
     if (novoNumero){
-        display.textContent = texto
-        novoNumero = false
+        display.textContent = texto;
+        novoNumero = false;
     } else {
-        display.textContent += texto
+        display.textContent += texto;
     }
 };
 
@@ -23,12 +40,16 @@ const inserirNumero = (evento) => {
 numeros.forEach(numero => numero.addEventListener('click', inserirNumero));
 //Isso dentro do .forEach é uma arrow function. 
 
-function selecionarOperador () {
-    novoNumero = true
+function selecionarOperador (event) { 
+    if (!novoNumero){
+        calcular();
+        novoNumero = true;
+        operador = event.target.textContent;
+        input1 = parseFloat(display.textContent);
+    }
 }
 
 operadores.forEach(operador => operador.addEventListener('click', selecionarOperador));
 
 
-// Paramos no minuto 20:40 = https://www.youtube.com/watch?v=oRZQ5EZOrQk&list=PLDgemkIT111AzoS1rB61sgMJbsEA4pyD2&index=7
-
+// Paramos no minuto 31:40 = https://www.youtube.com/watch?v=oRZQ5EZOrQk&list=PLDgemkIT111AzoS1rB61sgMJbsEA4pyD2&index=7
